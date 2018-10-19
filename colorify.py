89082774769855e2fa13916413f97825f07d9f5e
TralahM <mmtiese@gmail.com>
2018-10-20 02:56:23 +0300

    html file of sheetke0.ipynb

--- colorify.py
+++ colorify.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-from random import choice
+from random import choice,shuffle
 
 #Text color Ascii representations.
 Asciifg=['\033[3%dm'%i for i in range(1,8)]
 Asciifg+=['\033[9%dm'%i for i in range(1,8)]
-
+shuffle(Asciifg)
 #print (Asciifg)
 #Background color representations.
 Asciibg=['\033[4%dm'%j for j in range(1,6)]
-
+shuffle(Asciibg)
 #print (Asciibg)
 #default normal
 Norm='\033[0m\n'
